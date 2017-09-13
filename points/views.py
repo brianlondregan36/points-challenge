b4f158b54858588ddcb7d5b5b01a7c89a6a39d34
Brian Londregan <brian.londregan@gmail.com>
2017-09-13 18:01:30 -0400

    more tabbing issues

--- points/views.py
+++ points/views.py
@@ -8,53 +8,53 @@ import urllib2
 @app.route('/')
 def index():
 
-	bTeams = ["New Orleans", "Atlanta", "Pittsburgh", "New England", "Dallas", "Oakland", "Tennessee", "Kansas City", "Tampa Bay", "LA Chargers", "LA Rams", "Philadelphia", "Jacksonville", "Cleveland", "Minnesota", "Buffalo"]
-	sTeams = ["Carolina", "Arizona", "Seattle", "NY Giants", "Denver", "Chicago", "Green Bay", "San Francisco", "Cincinnati", "Washington", "Indianapolis", "Baltimore", "Miami", "Houston", "Detroit", "NY Jets"]
+    bTeams = ["New Orleans", "Atlanta", "Pittsburgh", "New England", "Dallas", "Oakland", "Tennessee", "Kansas City", "Tampa Bay", "LA Chargers", "LA Rams", "Philadelphia", "Jacksonville", "Cleveland", "Minnesota", "Buffalo"]
+    sTeams = ["Carolina", "Arizona", "Seattle", "NY Giants", "Denver", "Chicago", "Green Bay", "San Francisco", "Cincinnati", "Washington", "Indianapolis", "Baltimore", "Miami", "Houston", "Detroit", "NY Jets"]
 
-	url = "http://www.espn.com/nfl/statistics/team/_/stat/total/year/2017"
-	page = urllib2.urlopen(url)
-	soup = BeautifulSoup(page.read())
+    url = "http://www.espn.com/nfl/statistics/team/_/stat/total/year/2017"
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page.read())
 
 
-	topContainer = soup.find(id='my-teams-table')
+    topContainer = soup.find(id='my-teams-table')
 
-	tables = topContainer.find_all("table", class_="tablehead")
-	if len(tables) == 1:
-		table = tables[0]
-	else:
-		print("could not locate main table")
-	rows = table.find_all("tr")
+    tables = topContainer.find_all("table", class_="tablehead")
+    if len(tables) == 1:
+        table = tables[0]
+    else:
+        print("could not locate main table")
+    rows = table.find_all("tr")
 
-	for thisRow in rows:
+    for thisRow in rows:
 
-		headerCheck = thisRow.attrs['class']
-		if headerCheck[0] == "colhead":
-			continue
+        headerCheck = thisRow.attrs['class']
+        if headerCheck[0] == "colhead":
+            continue
 
-		cells = thisRow.find_all("td")
+        cells = thisRow.find_all("td")
 
-		thisTeamName = cells[1].find_all("a")[0].string
-		thisTeamPoints = int(cells[8].string)
+        thisTeamName = cells[1].find_all("a")[0].string
+        thisTeamPoints = int(cells[8].string)
 
-		for x in bTeams:
- 			if x == thisTeamName:
+        for x in bTeams:
+             if x == thisTeamName:
 
-				bTeams.remove(x)
-				bTeams.append(thisTeamPoints)
-				break
-		for y in sTeams:
-			if y == thisTeamName:
-				sTeams.remove(y)
- 				sTeams.append(thisTeamPoints)
- 				break
+                bTeams.remove(x)
+                bTeams.append(thisTeamPoints)
+                break
+        for y in sTeams:
+            if y == thisTeamName:
+                sTeams.remove(y)
+                 sTeams.append(thisTeamPoints)
+                 break
 
-	'''remove this after week 2'''
-	bTeams.remove(bTeams[0])
-	sTeams.remove(sTeams[0])
-	'''remove this after week 2'''
+    '''remove this after week 2'''
+    bTeams.remove(bTeams[0])
+    sTeams.remove(sTeams[0])
+    '''remove this after week 2'''
 
-	results = {}
-	results["Brian"] = sum(bTeams)
-	results["Sam"] = sum(sTeams
+    results = {}
+    results["Brian"] = sum(bTeams)
+    results["Sam"] = sum(sTeams
 
-	return render_template("index.html", rows=results)
+    return render_template("index.html", rows=results)
